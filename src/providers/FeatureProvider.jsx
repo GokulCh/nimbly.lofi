e113ff09a93e8680e1b4d41430eb72b6ad02dbc4
GokulCh <gokul.chaluvadi@gmail.com>
2025-05-19 21:41:27 -0400

    Soft reset

--- src/providers/FeatureProvider.jsx
+++ src/providers/FeatureProvider.jsx
@@ -1,18 +1,3 @@
-import { FocusProvider } from "../features/focus/FocusProvider";
-import { ChatProvider } from "../features/chat/ChatProvider";
-import { MusicProvider } from "../features/music/MusicProvider";
-
-import ToolBar from "../features/toolbar/ToolBar";
-
 export default function FeatureProvider({ children }) {
-  return (
-    <>
-      <FocusProvider>
-        {children}
-        <ToolBar />
-        <ChatProvider />
-        <MusicProvider />
-      </FocusProvider>
-    </>
-  );
+  return <></>;
 }
