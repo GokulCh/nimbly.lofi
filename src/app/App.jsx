48a2d412004c71458159bb915612d0cec887fe94
GokulCh <gokul.chaluvadi@gmail.com>
2025-05-19 22:59:17 -0400

    added dependency and refactor App component structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,18 @@
-import "./styles/App.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return <></>;
-}
+import CoreProvider from "../core/CoreProvider";
+import FeatureProvider from "../features/FeatureProvider";
 
-export default App;
+import "./styles/main.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CoreProvider>
+      <FeatureProvider>
+        <App />
+      </FeatureProvider>
+    </CoreProvider>
+  </React.StrictMode>
+);
